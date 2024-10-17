3e5c4e3bb2f88d0bb9a904219e35e793df64fe24
Akshay Shinde <akshay.shinde@lockated.com>
2024-10-18 00:05:10 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,  // Exposes server to network (0.0.0.0)
+    port: 3000,  // Use a non-privileged port
+    strictPort: true, // Fail if port 3000 is not available
+  },
 })
