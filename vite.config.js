31db5bfb32d2a9249598d27d859a1b06b48142b6
Akshay Shinde <akshay.shinde@lockated.com>
2024-10-18 13:53:18 +0530

    vite.config.js updated

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true, // Exposes server to the network (0.0.0.0)
+    port: 3000, // Use a non-privileged port
+    strictPort: true, // Fail if port 3000 is not available
     proxy: {
       '/api': {
-        target: 'https://marathon.lockated.com', 
+        target: 'https://marathon.lockated.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
