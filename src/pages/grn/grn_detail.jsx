de50be693a7b8c21b7239dc012bba969df3b133e
Akshay Shinde <akshay.shinde@lockated.com>
2024-11-18 11:27:49 +0530

    src/pages/grn/grn_detail.jsx

--- src/pages/grn/grn_detail.jsx
+++ src/pages/grn/grn_detail.jsx
@@ -758,6 +758,7 @@ const GoodReceiveNoteDetails = () => {
                         defaultValue={""}
                         value={comments}
                         onChange={handleCommentsChange}
+                        disabled
                       />
                     </div>
                   </div>
@@ -773,6 +774,22 @@ const GoodReceiveNoteDetails = () => {
                         defaultValue={""}
                         value={remarks}
                         onChange={handleRemarksChange}
+                        disabled
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="row mt-2">
+                  <div className="col-md-12">
+                    <div className="form-group">
+                      <label>Comments</label>
+                      <textarea
+                        className="form-control"
+                        rows={3}
+                        defaultValue={""}
+                        value={comments}
+                        onChange={handleCommentsChange}
+                        disabled
                       />
                     </div>
                   </div>
