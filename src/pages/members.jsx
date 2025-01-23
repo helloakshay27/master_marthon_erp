2b2eeb2c91cfb5bd3e81fe142fa938145973d4a1
fardeen-mohd-rafiq <fardeen2903@gmail.com>
2025-01-23 16:51:13 +0530

    fix: boq links on top

--- src/pages/members.jsx
+++ src/pages/members.jsx
@@ -251,6 +251,28 @@ function Members() {
         <div className="website-content overflow-auto">
           <div className="module-data-section container-fluid">
             <div className="m-3">
+              <h1>BOQ</h1>
+              <ul>
+                <li>
+                  <Link to="/create-BOQ">Create BOQ</Link>
+                </li>
+                <li>
+                  <Link to="/view-BOQ">View BOQ</Link>
+                </li>
+                <li>
+                  <Link to="/boq-approval-list">BOQ Approval List</Link>
+                </li>
+                <li>
+                  <Link to="/boq-details-page-master">
+                    BOQ Details Page Master
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/boq-approval-details">BOQ Approval Details</Link>
+                </li>
+              </ul>
+            </div>
+            <div className="m-3">
               <h1>RFQ Module</h1>
               <ul>
                 <li>
@@ -297,69 +319,59 @@ function Members() {
                   <Link to="/Wo">Wo list</Link>
                 </li>
                 <li>
-                <Link to="/user-lists">user_list</Link>
-              </li>
-
-              <li>
-                <Link to="/user-overview">user_overview</Link>
-              </li>
+                  <Link to="/user-lists">user_list</Link>
+                </li>
 
                 <li>
-                <Link to="good_receive_notes/:id">grn_detaisl</Link>
-              </li>
+                  <Link to="/user-overview">user_overview</Link>
+                </li>
 
                 <li>
-                <Link to="/stock_register_detail/47">stock_register detail</Link>
-              </li>
+                  <Link to="good_receive_notes/:id">grn_detaisl</Link>
+                </li>
+
                 <li>
-                <Link to="/erp-stock-register-creation13c">erp_stock_register_creation13c</Link>
-              </li>
+                  <Link to="/stock_register_detail/47">
+                    stock_register detail
+                  </Link>
+                </li>
                 <li>
-                <Link to="/stock_register_list?token=4ad0c1cd2506a717ae19ed050c28d7f078b0210991571e47"> stock register list</Link>
-              </li>
-              <li>
-                <Link to="/approvals-list">approvals_list</Link>
-              </li>
+                  <Link to="/erp-stock-register-creation13c">
+                    erp_stock_register_creation13c
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/stock_register_list?token=4ad0c1cd2506a717ae19ed050c28d7f078b0210991571e47">
+                    {" "}
+                    stock register list
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/approvals-list">approvals_list</Link>
+                </li>
               </ul>
             </div>
 
             <div className="m-3">
               <h1>RFQ OLD</h1>
               <ul>
-              <li>
-                <Link to="/create-rfq">create_rfq</Link>
-              </li>
-              <li>
-                <Link to="/erp-rfq-auction-events-4f">erp_rfq_auction_events_4f</Link>
-              </li>
-              <li>
-                <Link to="/erp-rfq-auction-events-4h">erp_rfq_auction_events_4h</Link>
-              </li>
-              <li>
-                <Link to="/erp-rfq-detail-price-trends4h">erp_rfq_detail_price_trends4h</Link>
-              </li>
-              </ul>
-            </div>
-
-            <div className="m-3">
-              <h1>BOQ</h1>
-              <ul>
                 <li>
-                  <Link to="/create-BOQ">Create BOQ</Link>
+                  <Link to="/create-rfq">create_rfq</Link>
                 </li>
                 <li>
-                  <Link to="/view-BOQ">View BOQ</Link>
-                </li>
-                <li>
-                  <Link to="/boq-approval-list">BOQ Approval List</Link>
-                </li>
-                <li>
-                  <Link to="/boq-details-page-master">
-                    BOQ Details Page Master
+                  <Link to="/erp-rfq-auction-events-4f">
+                    erp_rfq_auction_events_4f
                   </Link>
                 </li>
                 <li>
-                  <Link to="/boq-approval-details">BOQ Approval Details</Link>
+                  <Link to="/erp-rfq-auction-events-4h">
+                    erp_rfq_auction_events_4h
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/erp-rfq-detail-price-trends4h">
+                    erp_rfq_detail_price_trends4h
+                  </Link>
                 </li>
               </ul>
             </div>
