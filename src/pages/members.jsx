1f1a68c6b52465f25ca6e22a9e231b81acd24c1b
PriyaMane7 <priyamane5600@gmail.com>
2025-05-17 18:01:50 +0530

    verification,bill update

--- src/pages/members.jsx
+++ src/pages/members.jsx
@@ -419,9 +419,9 @@ function Members() {
                 <li>
                   <Link to="/credit-note-list">Credit Note List</Link>
                 </li>
-                <li>
+                {/* <li>
                   <Link to="/test-table">Test Table</Link>
-                </li>
+                </li> */}
                 <h5>___Debit Note___</h5>
                 <li>
                   <Link to="/debit-note-create">Debit Note Create</Link>
